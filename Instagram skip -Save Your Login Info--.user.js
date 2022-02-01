cdf769aa38caccc6f49e5ed3ccba62c05e7bbd21
petkeeper18 <96896686+petkeeper18@users.noreply.github.com>
2022-02-01 15:50:44 -0800

    Create Instagram skip -Save Your Login Info--.user.js

---
+++ Instagram skip -Save Your Login Info--.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name         Instagram skip "Save Your Login Info?"
+// @version      1.0
+// @description  redirect past "Save Your Login Info?"
+// @match        *www.instagram.com/accounts/onetap/*
+// ==/UserScript==
+
+window.location.replace("http://www.instagram.com");
