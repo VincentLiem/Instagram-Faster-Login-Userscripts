123bdc83f60dd48b8d84939f883e4f6457821d2b
petkeeper18 <96896686+petkeeper18@users.noreply.github.com>
2022-02-02 18:13:46 -0800

    Update Instagram skip -Save Your Login Info--.user.js

--- Instagram skip -Save Your Login Info--.user.js
+++ Instagram skip -Save Your Login Info--.user.js
@@ -3,6 +3,7 @@
 // @version      1.0
 // @description  redirect past "Save Your Login Info?"
 // @match        *www.instagram.com/accounts/onetap/*
+// @run-at       document-start
 // ==/UserScript==
 
 window.location.replace("https://www.instagram.com");
