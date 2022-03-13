82fe45f86c8416fb686b720bdcab21f5f1de9e87
VincentLiem <96896686+VincentLiem@users.noreply.github.com>
2022-03-13 13:19:46 -0700

    Create Skip Turn On Notifications Prompt on Login.js

---
+++ Skip Turn On Notifications Prompt on Login.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name         Skip Turn On Notifications Prompt on Login
+// @version      1.0
+// @match        https://www.instagram.com/*
+// @run-at       document-idle
+// ==/UserScript==
+
+document.getElementsByClassName("aOOlW   HoLwm ")[0].click();
