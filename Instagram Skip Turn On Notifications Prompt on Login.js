c1ddf43e3b7c6aced7f51bf1db8345d75357bb0f
VincentLiem <96896686+VincentLiem@users.noreply.github.com>
2022-03-19 10:49:03 -0700

    Update Instagram Skip Turn On Notifications Prompt on Login.js

--- Instagram Skip Turn On Notifications Prompt on Login.js
+++ Instagram Skip Turn On Notifications Prompt on Login.js
@@ -2,7 +2,7 @@
 // @name         Instagram Skip Turn On Notifications Prompt on Login
 // @version      1.0
 // @match        https://www.instagram.com/*
-// @run-at       document-end
+// @run-at       document-idle
 // ==/UserScript==
 
 document.getElementsByClassName("aOOlW   HoLwm ")[0].click(); //autopress don't allow
