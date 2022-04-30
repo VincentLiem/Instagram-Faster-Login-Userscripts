9c6158f36afd6e4b96bcb200334fc8b036cd6a0d
Vincent Liem <96896686+VincentLiem@users.noreply.github.com>
2022-04-29 23:46:12 -0700

    Create Instagram Theme Switcher.js

---
+++ Instagram Theme Switcher.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         Instagram Theme Switcher
+// @version      1.0
+// @description  Press F2 to change between dark and light theme
+// @match        https://www.instagram.com/*
+// @run-at       document-idle
+// ==/UserScript==
+
+
+// ==/UserScript==
+
+var currentUrl = window.location.href
+
+if (document.URL.indexOf("/?theme=dark") < 0)
+{
+    window.addEventListener('keydown', function (a) {
+        if (a.which == 113) {
+            window.location.replace(currentUrl + '/?theme=dark');
+        }
+    })
+}
+
+if (document.URL.indexOf("/?theme=dark") >= 0)
+{
+    window.addEventListener('keydown', function (a) {
+        if (a.which == 113) {
+            window.location.replace(window.location.href.replace("/?theme=dark", ""));
+        }
+    })
+}
